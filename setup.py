8c7d287021a47b8e442e14446a671f5441af8eaa
Karthik Bharadhwaj <karthik.bharadhwaj1998@gmail.com>
2024-07-06 17:14:14 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="end_to_end_ML_project",
+    version="0.0.0",
+    author="KarthikBharadwajKB",
+    author_email="karthik.bharadwaj1998@gmail.com",
+    packages=find_packages()
+)
